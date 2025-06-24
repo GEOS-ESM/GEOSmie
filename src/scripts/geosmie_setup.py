fc5abe9e42ab7fe93e3aa749261e9358e465739a
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-06-24 17:43:05 -0400

    modify setup to use existing scripts

--- src/scripts/geosmie_setup.py
+++ src/scripts/geosmie_setup.py
@@ -28,10 +28,10 @@ def print_message():
     The script is interactive and asks the user to provide:
 
         - an experiment name (exp_name)
-        - the group id (group_id), i.e., the NCCS sponsor code to be used in SLURM.
+        - the run script you want to start from
  
     It will then create an experiment directory that has a self-contained and ready
-    to use SLURM script geosmis_run.j.
+    to use run script
     ---------------------------------------------------------------------------------
     """
     print(mssg)
@@ -72,12 +72,6 @@ def search_reaplace_in_file(loc_filename: str,
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def ignore_top_level(dir, files):
-    # Ignore the source directory itself
-    if dir == source_directory:
-        return [source_directory]  # Return a list containing the source directory name
-    return [] # Don't ignore anything else
-
 
 def create_experiment_directory():
 
@@ -87,7 +81,7 @@ def create_experiment_directory():
 
     # Determine the source code main directory
     # Will be FULL_PATH/GEOSmie
-    source_directory = current_directory.parent.parent.parent
+    source_directory = current_directory.parent.parent
 
     reference_directory = source_directory.parent
 
@@ -116,54 +110,37 @@ def create_experiment_directory():
     shutil.copytree(config_filepath, experiment_directory / config_filepath.name,dirs_exist_ok=True)
 
     # Copy geosmie scripts to the experiment directory
-    config_filepath = current_directory.parent / "geosmie/*"
+    config_filepath = current_directory / "geosmie/*"
     for p in glob.glob(str(config_filepath)):
         if os.path.isfile(p):
             shutil.copy(p, experiment_directory)
         elif os.path.isdir(p):
             shutil.copytree(p, experiment_directory / os.path.basename(p),dirs_exist_ok=True)
 
+    # Copy gsf scripts to the experiment directory
+    config_filepath = current_directory.parent / "gsf/*"
+    for p in glob.glob(str(config_filepath)):
+        if os.path.isfile(p):
+            shutil.copy(p, experiment_directory)
+        elif os.path.isdir(p):
+            shutil.copytree(p, experiment_directory / os.path.basename(p),dirs_exist_ok=True)
 
-    # Get the sponsor code id
+    # Get the template script
+    nscript = "proc.v2.0.0.csh"
+    script_name = input(f"Provide the script name [default:{nscript}]:  ")
+    script_name = script_name.strip()
+    if not script_name:
+        script_name = nscript
 
-    result = subprocess.run(["groups"], shell=True, capture_output=True, text=True)
-
-    groups = result.stdout.strip().split()
-
-    print(f"The list of available group ids is: \n\n\t {result.stdout.strip()}")
-    print()
-    my_group = input(f"Provide the group id do you want to use (default: {groups[0]}): ")
-    my_group = my_group.strip()
-
-    if not my_group:
-        my_group = groups[0]
-
-    print()
-    print(f"Your group is is: {my_group}")
-    print()
-
-    if my_group not in groups:
-        print(f"You selected and invalid group.")
-        print(f"The group {groups[0]} will be used.")
-        print(f"You can change the group id in the SLURM script available in the experiment directory")
-        print()
-
-    loc_filename = "geosmie_runoptics.j"
     target_dir = experiment_directory
-    dict_words = {"@SRCDIR": str(source_directory), "@GROUPID": my_group}
-    search_reaplace_in_file(loc_filename, target_dir, dict_words)
-
-    loc_filename2 = "geosmie_runbands.j"
-    target_dir = experiment_directory
-    dict_words = {"@SRCDIR": str(source_directory), "@GROUPID": my_group}
-    search_reaplace_in_file(loc_filename2, target_dir, dict_words)
-
+    dict_words = {"@SRCDIR": str(source_directory)}
+    search_reaplace_in_file(script_name, target_dir, dict_words)
 
     print()
     print("-"*70)
     print(f"The experiment directory was created: \n\n\t {experiment_directory}")
     print()
-    print(f"Go to the folder and if necessary edit the file {loc_filename} or {loc_filename2}.")
+    print(f"Go to the folder and if necessary edit the file {script_name}.")
     print("-"*70)
     print()
 
