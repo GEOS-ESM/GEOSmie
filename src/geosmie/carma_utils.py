a40fdd3049365183b8a82a7d438c7e723518c3c2
Peter Colarco <peter.r.colarco@nasa.gov>
2025-12-04 16:09:03 -0500

    prc: revert to default python

--- src/geosmie/carma_utils.py
+++ src/geosmie/carma_utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.11
+#!/usr/bin/env python3
 
 # Utilities for calculating properties for CARMA
 import numpy as np
--- src/utils/plotoptics_bins_legacy.py
+++ src/utils/plotoptics_bins_legacy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.11
+#!/usr/bin/env python3
 """
 Read a geosmie optics table
 Plot basic single scattering properties
